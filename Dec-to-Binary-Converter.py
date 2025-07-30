218fde1be71bfdfe4ee50abc5f5fd69ee2393354
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-07-30 15:25:08 +0100

    Update Dec-to-Binary-Converter.py

--- Dec-to-Binary-Converter.py
+++ Dec-to-Binary-Converter.py
@@ -1,9 +1,9 @@
 def ConvertToBin(number): 
   if (number < 64): 
-      print(bin(number))
+      print(bin(number)[2:])
       print('Lower Than Eight Digits')
   else: 
-    print(bin(number)) 
+    print(bin(number)[2:]) 
     print('Greater than Eight')
     
 ConvertToBin(int(input('Enter your Number: ')))

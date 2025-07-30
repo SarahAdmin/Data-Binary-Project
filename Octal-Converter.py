6a4ce8a867bf1d106cb400012e726ff6f1fc2a26
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-07-30 15:26:33 +0100

    Update Octal-Converter.py

--- Octal-Converter.py
+++ Octal-Converter.py
@@ -3,6 +3,6 @@ def ConvertDectoOct(number):
   return oct_representation 
   
 if __name__ == "__main__": 
-  user_one = intput('Enter a number: ') 
+  user_one = input('Enter a number: ') 
   oct_output = ConvertDectoOct(user_one) 
   print(f'In Octal: {oct_output}')
